193e8a27f7f39fa05cb6885463894fc7576f642b
ShadiraDelgado03 <shadira.delgado@idslatam.com>
2026-03-10 16:32:18 -0500

    Literalura challenge completed with Spring Boot and PostgreSQL

---
+++ src/main/java/com/literalura/literalura/LiteraturaApplication.java
@@ -0,0 +1,26 @@
+package com.literalura.literalura;
+
+
+import com.literalura.literalura.principal.Principal;
+import com.literalura.literalura.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraturaApplication.class, args);
+    }
+
+    @Autowired
+    private MenuService menuService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(menuService);
+        principal.EjecutarAplicacion();
+    }
+}
